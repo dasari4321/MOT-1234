5ff0cdb55c5b830a41f2cf167e99ce2bada2394b
dasari4321 <43631298+dasari4321@users.noreply.github.com>
2018-09-27 12:53:15 +0530

    Add files via upload

--- utils.py
+++ utils.py
@@ -5,8 +5,21 @@ Created on Thu Sep 27 11:46:22 2018
 
 @author: sudha
 """
-import pims
+import numpy as np
+import cv2
 
 def read_video(s):
-    v=pims.Video(s)
-    return v
+    cap = cv2.VideoCapture(s)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def write_video(s):
+    
